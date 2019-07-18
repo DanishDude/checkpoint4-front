90da05f7c95df3a5d976c5efb23b489fdbc7ca32
DanishDude <christian.hinge@gmail.com>
2019-07-18 09:13:33 +0200

    wip edit btn

--- src/containers/Admin/Programe.jsx
+++ src/containers/Admin/Programe.jsx
@@ -3,77 +3,74 @@ import { CardDeck, CardImg, Card, CardBody, CardText, CardTitle, Button, } from 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 
-import {asyncFetchProgrames} from '../../actions/fetchPrograme';
+import { asyncFetchProgrames } from '../../actions/fetchPrograme';
 import NewPrograme from './NewPrograme';
 
 class Programe extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
 
   componentDidMount() {
     const { asyncFetchProgrames } = this.props;
     asyncFetchProgrames();
   }
-  
+
   handleDelete(id) {
-    console.log('toto');
-    console.log(id);
+    if (window.confirm("Are you sure you want to delete this act?")) {
+      fetch(`http://localhost:5000/api/programe/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json", },
+        body: JSON.stringify({ id })
+      })
+        .then(res => {
+          if (res.error) {
+            alert(res.error);
+          } else {
+            window.location.reload();
+          }
 
-    if (window.confirm("Are you sure you wan to delete this act?")) {
-      fetch(`http://localhost:5000/api/programe/${id}`, { 
-      method: "DELETE",
-      headers: { "Content-Type": "application/json", },
-      body: JSON.stringify({id})
-    })
-      .then(res => {
-        if (res.error) {
-          alert(res.error);
-        } else {
-          window.location.reload();
-        }
+        }).catch(event => {
+          console.error(event);
+          alert('Ah Snap :-/ this act was not deleted. Please try again later')
+        });
 
-      }).catch(event => {
-        console.error(event);
-        alert('Ah Snap :-/ this act was not deleted. Pls try again later')
-      });
-
-  } else {
+    } else {
       alert("Nothing was deleted")
-  }
+    }
   }
 
   render() {
     const { programes, loading, error } = this.props;
     console.log(programes);
-    return ( 
+    return (
       <div className="Programe">
         <NewPrograme />
         <div className="acts">
 
           {error !== '' ? <div>{error}</div> : ''}
-        {loading ? <div>Loading programes ...</div> : (
-          <CardDeck className="container row">
-          {(programes && programes.length > 0)
-          ? programes.map((programe, index) => (
-            <Card key={index} className="col-4">
-                <CardImg top width="100%" src={programe.image} alt="Card image cap" />
-                <CardBody>
-                  <CardTitle>{programe.title}</CardTitle>
-                  <CardText>{programe.description}</CardText>
-                  <Button color="danger" onClick={() => this.handleDelete(programe.id)}>Delete</Button>
-                </CardBody>
-              </Card>
-          ))
-          : <div> There are no programes ... </div>
-          }
-          </CardDeck>
-        )}
+          {loading ? <div>Loading programes ...</div> : (
+            <CardDeck className="container row">
+              {(programes && programes.length > 0)
+                ? programes.map((programe, index) => (
+                  <Card key={index} className="col-4">
+                    <CardImg top width="100%" src={programe.image} alt="Card image cap" />
+                    <CardBody>
+                      <CardTitle>{programe.title}</CardTitle>
+                      <CardText>{programe.description}</CardText>
+                      <Button color="danger" onClick={() => this.handleDelete(programe.id)}>Delete</Button>
+                    </CardBody>
+                  </Card>
+                ))
+                : <div> There are no programes ... </div>
+              }
+            </CardDeck>
+          )}
 
+        </div>
       </div>
-      </div>
-     );
+    );
   }
 }
 
